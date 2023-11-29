456a5d7dc4c13102e57f72e6ea7fa615d1c3e3a0
Simon <simon@aam-digital.com>
2023-11-29 10:08:17 +0100

    added endpoint to query report config using provided auth

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,13 +1,38 @@
-import { Controller, } from '@nestjs/common';
+import { Controller, Get, Headers, HttpException, Param } from '@nestjs/common';
 import { HttpService } from '@nestjs/axios';
 import { ConfigService } from '@nestjs/config';
+import { ApiHeader } from '@nestjs/swagger';
+import { catchError, map } from 'rxjs';
 
 @Controller()
 export class AppController {
-
+  dbUrl = this.configService.get('DATABASE_URL');
+  dbUsername = this.configService.get('DATABASE_USERNAME');
+  dbPassword = this.configService.get('DATABASE_PASSWORD');
+  queryUrl = this.configService.get('QUERY_URL');
   constructor(
     private http: HttpService,
     private configService: ConfigService,
+  ) {}
+
+  // TODO also support cookie auth
+  @ApiHeader({ name: 'Authorization', required: false })
+  @Get(':id')
+  queryData(
+    @Param('id') reportId: string,
+    @Headers('Authorization') token: string,
   ) {
+    return this.http
+      .get(`${this.dbUrl}/app/ReportConfig:${reportId}`, {
+        headers: { Authorization: token },
+      })
+      .pipe(
+        map(({ data }) => data),
+        catchError((err) => {
+          throw err.response
+            ? new HttpException(err.response.data, err.response.status)
+            : err;
+        }),
+      );
   }
 }
