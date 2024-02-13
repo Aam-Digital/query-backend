d6553107da800e163ee96285dc67f3493ef8d2ea
Tom Winter <tom@aam-digital.com>
2024-02-13 08:46:38 +0100

    fix: remove swagger

--- src/main.ts
+++ src/main.ts
@@ -5,19 +5,6 @@ import { SentryService } from '@ntegral/nestjs-sentry';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // // SwaggerUI setup see https://docs.nestjs.com/openapi/introduction#bootstrap
-  // const config = new DocumentBuilder()
-  //   .setTitle(process.env.npm_package_name)
-  //   .setDescription(process.env.npm_package_description)
-  //   .setVersion(process.env.npm_package_version)
-  //   .addBearerAuth(undefined, 'BearerAuth')
-  //   .addSecurityRequirements('BearerAuth')
-  //   .build();
-  // const document = SwaggerModule.createDocument(app, config);
-  // SwaggerModule.setup('api', app, document, {
-  //   swaggerOptions: { persistAuthorization: true },
-  // });
-
   // Logging everything through sentry
   app.useLogger(SentryService.SentryServiceInstance());
 
