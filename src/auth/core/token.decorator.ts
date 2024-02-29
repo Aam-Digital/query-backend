02ffc00640431512f5abd4d54e9d5b565f19869e
Tom Winter <mail@tom-winter.com>
2024-02-29 10:13:12 +0100

    Update src/auth/core/token.decorator.ts
    Co-authored-by: Sebastian <sebastian@aam-digital.com>

--- src/auth/core/token.decorator.ts
+++ src/auth/core/token.decorator.ts
@@ -1,6 +1,9 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 import { JwtTokenPayload } from './jwt-auth.guard';
 
+/**
+ * Extract token payload for advanced checks.
+ */
 export const Token = createParamDecorator(
   (data: unknown, ctx: ExecutionContext): JwtTokenPayload | undefined => {
     const request = ctx.switchToHttp().getRequest();
