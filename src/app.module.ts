721a53f28574bdad8f15ad65c9c3cae851e91fb7
Tom Winter <tom@aam-digital.com>
2024-02-29 10:58:05 +0100

    chore: remove merge relicts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,5 @@
-import { HttpException, Module } from '@nestjs/common';
-import { SentryInterceptor, SentryModule } from '@ntegral/nestjs-sentry';
-import { SeverityLevel } from '@sentry/types';
-import { APP_INTERCEPTOR } from '@nestjs/core';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { HttpModule } from '@nestjs/axios';
 import { ReportModule } from './report/report.module';
 import { ScheduleModule } from '@nestjs/schedule';
@@ -12,23 +9,7 @@ import { NotificationModule } from './notification/notification.module';
 import { QueryModule } from './query/query.module';
 import { AuthModule } from './auth/auth.module';
 
-const lowSeverityLevels: SeverityLevel[] = ['log', 'info'];
-
 @Module({
-  providers: [
-    {
-      provide: APP_INTERCEPTOR,
-      useFactory: () =>
-        new SentryInterceptor({
-          filters: [
-            {
-              type: HttpException,
-              filter: (exception: HttpException) => 500 > exception.getStatus(), // Only report 500 errors
-            },
-          ],
-        }),
-    },
-  ],
   imports: [
     HttpModule,
     ScheduleModule.forRoot(),
@@ -37,38 +18,8 @@ const lowSeverityLevels: SeverityLevel[] = ['log', 'info'];
       ignoreEnvFile: false,
       load: [AppConfiguration],
     }),
-    QueryModule,
     AuthModule,
-    SentryModule.forRootAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: async (configService: ConfigService) => {
-        if (!configService.get('SENTRY_DSN')) {
-          return {};
-        }
-
-        return {
-          dsn: configService.getOrThrow('SENTRY_DSN'),
-          debug: true,
-          environment: 'prod',
-          release: 'backend@' + process.env.npm_package_version,
-          whitelistUrls: [/https?:\/\/(.*)\.?aam-digital\.com/],
-          initialScope: {
-            tags: {
-              // ID of the docker container in which this is run
-              hostname: process.env.HOSTNAME || 'unknown',
-            },
-          },
-          beforeSend: (event) => {
-            if (lowSeverityLevels.includes(event.level as SeverityLevel)) {
-              return null;
-            } else {
-              return event;
-            }
-          },
-        };
-      },
-    }),
+    QueryModule,
     ReportModule,
     ReportChangesModule,
     NotificationModule,
